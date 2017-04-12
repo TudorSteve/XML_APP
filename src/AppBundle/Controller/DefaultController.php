38aaa77a0ff36768c3872afa087d0464775c3bae
Tudor Frincu <tudor.frincu@outlook.com>
2017-04-12 11:39:36 +0300

    initial commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
-    }
+class DefaultController extends Controller {
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function indexAction(Request $request) {
+		// $route = new Route();
+		// replace this example code with whatever you need
+		return $this->render('default/index.html.twig', array(
+				'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,'a'=>12,'b'=>5,
+			));
+	}
 }
